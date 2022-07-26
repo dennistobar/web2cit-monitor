df10f1702c236125726aa571c03b30192888d90c
Dennis Tobar <dennis.tobar@gmail.com>
2022-07-25 22:26:08 -0400

    feat: adding parse for scores

--- web2citwrapper/domain.py
+++ web2citwrapper/domain.py
@@ -10,6 +10,13 @@ class Domain(object):
 
     def score(self):
         tests = self.tests()
+        scores = {}
+        for test in tests:
+            local_fields = test.get('results').pop().get('fields')
+            scores[test.get('href')] = list(
+                map(lambda x: x.get('score'), local_fields))
+
+        return scores
 
     def __str__(self):
         return self.domain
